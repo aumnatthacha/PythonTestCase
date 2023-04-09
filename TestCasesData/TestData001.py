792b700c9d5db1c1ab6cfa127dd16a524fbcc71d
aumnatthacha <96460324+aumnatthacha@users.noreply.github.com>
2023-04-09 21:04:34 +0700

    ..

--- TestCasesData/TestData001.py
+++ TestCasesData/TestData001.py
@@ -1,27 +1,88 @@
+# from selenium import webdriver
+# from selenium.webdriver.common.keys import Keys
+# from selenium.webdriver.common.by import By
+# import time
+
+# driver = webdriver.Firefox()
+
+# driver.get('http://localhost:3000/index.php')
+# driver.set_window_size(784, 816)
+# # user = driver.find_element(By.XPATH, '//*[@id="formGroupExampleInput2"]')
+# # user.send_keys('ณาตหชา มุมแดง')
+# driver.find_element(By.XPATH, "//*[@id='formGroupExampleInput2']").send_keys('ณาตหชา มุมแดง')
+# driver.find_element(By.XPATH, "//form/input").send_keys('9/4/2566')
+# driver.find_element(By.XPATH, "//div[2]/input").send_keys('83')
+# time.sleep(5)
+# driver.find_element(By.XPATH, "//button[@name='submit']").click()
+# time.sleep(5)
+# assert "...done!" in driver.page_source
+# driver.close()
+
+import unittest
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-import time
-
-driver = webdriver.Firefox()
-driver.get("https://www.gmm-tv.com/shop/index.php?route=account/register")
-driver.set_window_size(1358, 782)
-driver.find_element(By.ID,'input-firstname').send_keys('ณาตหชา')
-driver.find_element(By.ID,'input-lastname').send_keys('มุมแดง')
-driver.find_element(By.ID,'input-email').send_keys('aummumdaeng@gmail.com')
-driver.find_element(By.ID,'input-password').send_keys('natthacha2002')
-driver.find_element(By.ID,'input-confirm').send_keys('natthacha2002')
-driver.find_element(By.XPATH,"//input[@name='agree']").click()
-time.sleep(5)
-driver.find_element(By.XPATH,"//input[@value='Submit / ลงทะเบียน']").click()
+import HtmlTestRunner
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
 
 
-# elem = driver.find_element(By.NAME, "q")
-# elem.clear()
-# elem.send_keys("pycon")
-time.sleep(3)
-# elem.send_keys(Keys.RETURN)
-assert "Telephone must be between 3 and 32 characters!"  in driver.page_source
-# assert "Telephone must be between 3 and 32 characters!"  in driver.page_source
-# time.sleep(5)
-driver.close()
+class testdata(unittest.TestCase):
+
+    def setUp(self):
+        self.driver = webdriver.Firefox()
+
+    def testdata(self):
+        driver = self.driver
+        driver.get("http://localhost:3000/index.php")
+        driver.set_window_size(1358, 782)
+        driver.find_element(
+            By.XPATH, "//*[@id='formGroupExampleInput2']").send_keys('ณาตหชา มุมแดง')
+        driver.find_element(By.XPATH, "//form/input").send_keys('2023/4/9')
+        driver.find_element(By.XPATH, "//div[2]/input").send_keys('83')
+        driver.find_element(By.XPATH, "//button[@name='submit']").click()
+        # driver.find_element(By.XPATH, "//body").click()
+        try:
+           WebDriverWait(driver, 3).until(EC.alert_is_present(),
+                                           '...done!')
+
+           alert = driver.switch_to.alert
+           alert.accept()
+           print("alert accepted")
+
+        except TimeoutException:
+            print("no alert")
+            # self.assertIn("...done!", driver.page_source)
+            # driver.find_element(By.XPATH,"//body").click()
+        driver.close()
+
+    def tearDown(self):
+        # self.driver.close()
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main(testRunner=HtmlTestRunner.HTMLTestRunner())
+
+
+# from selenium import webdriver
+# from selenium.webdriver.support.ui import WebDriverWait
+# from selenium.webdriver.support import expected_conditions as EC
+# from selenium.common.exceptions import TimeoutException
+
+# browser = webdriver.Firefox()
+# browser.get("url")
+# browser.find_element_by_id("add_button").click()
+
+# try:
+#     WebDriverWait(browser, 3).until(EC.alert_is_present(),
+#                                    'Timed out waiting for PA creation ' +
+#                                    'confirmation popup to appear.')
+
+#     alert = browser.switch_to.alert
+#     alert.accept()
+#     print("alert accepted")
+# except TimeoutException:
+#     print("no alert")
